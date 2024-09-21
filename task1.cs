8644d3a2252a4abb241ce595d2b0d6059e57d860
DanilCode <perez3498145@gmail.com>
2024-09-21 23:06:26 +0600

    Add files via upload

---
+++ task1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main()
+    {
+        int a = 20, b = 15;
+        a += b;
+        b = a - b;
+        a -= b;
+        Console.WriteLine(a + "\n" + b);
+    }
+}
