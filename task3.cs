2a9e5c1fb81a943f6773b7bce3220bb03eef2307
DanilCode <perez3498145@gmail.com>
2024-09-22 23:20:39 +0600

    commit

---
+++ task3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace forpractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            int l = 3, m = 3, p = 5, n = 10; //результаты при подстановке различных n совпадают
+            Console.WriteLine(n * (2 * (p + m) + (l * (n + 1)))); 
+        }
+    }
+}
