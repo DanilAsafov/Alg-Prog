f4fec7846d7ec52d7f60b7d23287636e319f31de
DanilCode <perez3498145@gmail.com>
2025-05-21 23:28:49 +0600

    Add files via upload

---
+++ Term 2/lw10.2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class FileOddNumberFilter {
+    public void FilterLinesWithOddNumbers(string inputFilePath, string outputFilePath) {
+        string[] lines = File.ReadAllLines(inputFilePath);
+        var filteredLines = lines.Where(ContainsOddNumber).ToArray();
+        File.WriteAllLines(outputFilePath, filteredLines);
+    }
+
+    private bool ContainsOddNumber(string line) {
+        foreach (Match match in Regex.Matches(line, @"\d+")) {
+            if (long.TryParse(match.Value, out long number) && number % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class Peogram {
+    static void Main() {
+        var filter = new FileOddNumberFilter();
+        filter.FilterLinesWithOddNumbers("input.txt", "output.txt");
+    }
+}
